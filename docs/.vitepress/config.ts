9d8c044cbc5733c30d0fc1cf2629da4760cde08f
lcyh <lc173408798@yeah.net>
2022-08-20 16:47:23 +0800

    init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,67 @@
+export default {
+  title: "kittyui", //站点标题
+  description: "一个vue3组件库", //mate标签description，多用于搜索引擎抓取摘要
+  themeConfig: {
+    siteTitle: "Kitty",
+    logo: "/logo.png",
+    socialLinks: [
+      { icon: "github", link: "https://github.com/lcyh/lcyh.github.io" },
+      { icon: "twitter", link: "https://www.baidu.com" },
+      // You can also add custom icons by passing SVG as string:
+      {
+        icon: {
+          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title><path d="M12...6.38z"/></svg>',
+        },
+        link: "https://www.baidu.com",
+      },
+    ],
+    nav: [
+      { text: "博客", link: "/articles/组件库环境搭建" },
+      { text: "GuideTest", link: "/guide/test" },
+      { text: "gitee", link: "https://gitee.com/geeksdidi" },
+      {
+        text: "Drop Menu",
+        items: [
+          {
+            items: [
+              { text: "Item A1", link: "/item-A1" },
+              { text: "Item A2", link: "/item-A2" },
+            ],
+          },
+          {
+            items: [
+              { text: "Item B1", link: "/item-B1" },
+              { text: "Item B2", link: "/item-B2" },
+            ],
+          },
+        ],
+      },
+    ],
+    sidebar: {
+      "/articles/": [
+        {
+          text: "组件库源码实现",
+          collapsible: true,
+          items: [
+            {
+              text: "组件库环境搭建",
+              link: "/articles/组件库环境搭建",
+            },
+            { text: "gulp的使用", link: "/articles/gulp的使用" },
+          ],
+        },
+        {
+          text: "vue教程",
+          collapsible: true,
+          collapsed: true,
+          items: [
+            {
+              text: "pina和vuex",
+              link: "/articles/pina和vuex",
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
---
+++ docs/index.md
@@ -0,0 +1,30 @@
+---
+layout: home
+
+hero:
+  name: lc的博客
+  text: 随便写点啥.
+  tagline: 帅气又迷人的小月
+  image:
+    src: /logo.png
+    alt: Kitty
+  actions:
+    - theme: brand
+      text: 快来快来
+      link: /articles/组件库环境搭建
+    - theme: alt
+      text: View on Gitee
+      link: https://gitee.com/geeksdidi
+
+features:
+  - icon: ⚡️
+    title: 这是一个闪电图标
+    details: wawawa
+  - icon: 🖖
+    title: 这是一个手掌图标
+    details: good...
+  - icon: 🛠️
+    title: 这是一个修理图标
+    details: cocococo
+---
+
